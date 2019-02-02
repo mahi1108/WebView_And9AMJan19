c02dfe4f3f25899f96c684b3ae004c206b7d62b6
maheshthippala <mahesh.cubex@gmail.com>
2019-02-02 11:00:29 +0530

    Updated...

--- app/src/main/java/cubex/mahesh/webview_and9amjan19/MainActivity.kt
+++ app/src/main/java/cubex/mahesh/webview_and9amjan19/MainActivity.kt
@@ -1,8 +1,13 @@
 package cubex.mahesh.webview_and9amjan19
 
+import android.app.ProgressDialog
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        wview.webViewClient = WebViewClient()
+        var pDialog = ProgressDialog(this@MainActivity)
+        pDialog.setTitle("Message")
+        pDialog.setMessage("Please wait page is loading...")
+        wview.webViewClient = object:WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                pDialog.show()
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                pDialog.dismiss()
+            }
+        } // WebView Client
+        wview.settings.javaScriptEnabled = true
+        wview.settings.builtInZoomControls = true
 
         search.setOnClickListener {
             wview.loadUrl(et1.text.toString())
@@ -27,7 +47,22 @@ class MainActivity : AppCompatActivity() {
 
             wview.loadUrl("http://www.youtube.com")
         }
-        html.setOnClickListener {  }
+        html.setOnClickListener {
+            wview.addJavascriptInterface(this@MainActivity,
+                "and9amjan19")
+            wview.loadUrl("file:///android_asset/login.html")
 
-    }
+        }
+
+    }  // onCreate
+
+        @JavascriptInterface
+        fun login(email:String, pass:String)
+        {
+            Toast.makeText(this@MainActivity,
+                email+"\n"+pass,Toast.LENGTH_LONG).show()
+        }
+
+
 }
+
